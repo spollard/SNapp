1551781a160fdce43e8288a1ba6cbd7c4fcd0189
Stephen Pollard <stephen@stephentpollard.com>
2022-01-02 14:10:03 -0500

    Adding all notations file

---
+++ client/src/notations/All.tsx
@@ -0,0 +1,21 @@
+// To add a new notation, copy the Simplified file, rename the file, rename
+// the exported variable, import it here, and add it to the list of notations here.
+
+// ALSO add it to the list of Notation options in ../context/preferences.tsx.
+// Ideally this would not be necessary, but I haven't figured out how to import
+// stuff into ../context/preferences yet....
+
+
+// Import your notation here
+import Traditional from './Traditional';
+import Simplified from './Simplified';
+import Solfege from './Solfege';
+
+// And add it here
+let Notations = {
+    Traditional,
+    Simplified,
+    Solfege,
+};
+
+export default Notations;
